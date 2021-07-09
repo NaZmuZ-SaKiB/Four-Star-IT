68fd95a16b81eaf8d16d631cb1b23da3d769b7f6
NaZmuZ-SaKiB <sakibnazmuz01@gmail.com>
2021-07-09 16:33:55 +0600

    added js in header top

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+// ----------------------------------------------------------------------------
+//                         making the header sticky
+// ----------------------------------------------------------------------------
+
 const headerTop = document.getElementById('header-top')
 
-console.log(headerTop)
+window.addEventListener('scroll', hideHeader)
+
+function hideHeader() {
+    if (window.scrollY > headerTop.offsetHeight + 50) {
+        headerTop.classList.add('hide-header-top')
+        headerTop.parentNode.style.width = '100%'
+        headerTop.parentNode.style.position = 'fixed'
+    } else {
+        headerTop.classList.remove('hide-header-top')
+        headerTop.parentNode.style = ''
+    }
+}
+
+// ----------------------------------------------------------------------------
+//                                  Ending
+// ----------------------------------------------------------------------------
